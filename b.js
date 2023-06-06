8ee76acd5c3d36c9def6fd18ba7305b28fdeb9cd
MivinaVanPrima <135717348+MivinaVanPrima@users.noreply.github.com>
2023-06-06 19:31:15 +0500

    lab2

---
+++ b.js
@@ -0,0 +1,20 @@
+let s = '<table class="table">' + "\n"
+s +=	"<tbody>" + "\n"
+s +=	"<tr>" + "\n"
+	s += 	"<th>" + " " + "</th>"
+	for (var i = 1; i <= 10; i++) {
+		s += "<th>" + String(i) + "</th>" + "\n"
+	}
+
+	s += 	"</tr>" + "\n"
+	for (var i = 1; i <= 10; i++) {
+		s += '<tr class="ya' + String(i) + '">' + "\n"
+		s += "<th>" + String(i) + "</th>"
+		for (var j = 1; j <= 10; j++) {
+			s += "<th>" + String(i*j) + "</th>" + "\n"
+		}
+	s += 	"</tr>" + "\n"
+	}
+s +=	"</tbody>" + "\n"
+s += 	"</table>"
+tb.innerHTML = s
